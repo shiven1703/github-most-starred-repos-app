dccddb314418839368201995fa6a3d4fba85b066
shiven1703 <shiven1703@gmail.com>
2021-02-26 20:42:16 +0530

    About page

---
+++ src/Pages/About.js
@@ -0,0 +1,13 @@
+import NavBar from "./../Components/NavBar";
+import PageIntro from "./../Components/PageIntro";
+
+function About() {
+  return (
+    <>
+      <NavBar />
+      <PageIntro title="About" subtitle="" />
+    </>
+  );
+}
+
+export default About;
