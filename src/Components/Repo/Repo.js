eca555ac5150b32a9314be359436da9e7e73ec5f
shiven1703 <shiven1703@gmail.com>
2021-02-27 17:47:45 +0530

    Integrated Github rest api

--- src/Components/Repo/Repo.js
+++ src/Components/Repo/Repo.js
@@ -1,22 +1,32 @@
-import './Repo.css';
+import "./Repo.css";
 
-function Repo() {
-    return (
-        <div className="card repo-card">
-            <div className="card-body">
-                <div className="row">
-                    <div className="col-md-6">
-                        <a href="/#"><span className="repo-title"> &#128213; liamg / traitor<br/></span></a>  
-                        <span className="repo-description ">Automatic Linux privesc via exploitation of low-hanging fruit e.g. gtfobins</span>
-                    </div>
-                    <div className="col-md-6 text-right">
-                        <span>By Liam Galvin</span><br/>
-                        <span>&#11088; 1,154</span>
-                    </div>
-                </div>
-            </div>
+function Repo(props) {
+  return (
+    <div className="card repo-card">
+      <div className="card-body">
+        <div className="row">
+          <div className="col-md-6">
+            <a
+              href={"https://github.com/" + props.data.full_name}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <span className="repo-title">
+                &#128213; {props.data.full_name}
+                <br />
+              </span>
+            </a>
+            <span className="repo-description ">{props.data.description}</span>
+          </div>
+          <div className="col-md-6 text-right">
+            <span className="stars">
+              &#11088; {props.data.stargazers_count}
+            </span>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Repo;
